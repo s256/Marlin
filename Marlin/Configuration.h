7ef4cba3677cdaa3d9f55f90fee7514cac026730
Sebastian Schneider <sebastian.schneider@boxine.de>
2021-05-27 20:58:20 +0200

    fix printer dimensions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1239,10 +1239,10 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 3
-#define Y_MIN_POS 0
+#define X_MIN_POS 0
+#define Y_MIN_POS -11
 #define Z_MIN_POS 0
-#define X_MAX_POS 223
+#define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 
